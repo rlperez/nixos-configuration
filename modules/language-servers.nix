fa424e350db223eb5cb928f99c74f0eac76dc124
Rigoberto L. Perez <rlperez@kablamo.me>
2026-07-15 23:10:44 -0400

    add some of the most likely lsps I use

--- modules/language-servers.nix
+++ modules/language-servers.nix
@@ -1,6 +1,16 @@
-{ pkgs, ... }: 
+{ pkgs, ... }:
+let
+  elm-ls = pkgs.elmPackages.elm-language-server;
+in
 {
   environment.systemPackages = with pkgs; [
+    clojure-lsp
+    elixir-ls
+    elm-ls
+    fish-lsp
+    jdt-language-server
     nixd
+    typescript-language-server
+    vscode-langservers-extracted
   ];
 }
