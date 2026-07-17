e97a076657e87e2094090022d813ee57a8190afc
Rigoberto L. Perez <rlperez@kablamo.me>
2026-07-16 23:01:50 -0400

    fix kmail to work with gmail oauth

--- modules/productivity.nix
+++ modules/productivity.nix
@@ -2,11 +2,20 @@
 let
   kmail = pkgs.kdePackages.kmail;
   kmail-account-wizard = pkgs.kdePackages.kmail-account-wizard;
+  kaccounts-integration = pkgs.kdePackages.kaccounts-integration;
+  signon-kwallet-extension = pkgs.kdePackages.signon-kwallet-extension;
+  signond = pkgs.kdePackages.signond;
 in
 {
   environment.systemPackages = with pkgs; [
+    # KMail and dependencies
     kmail
     kmail-account-wizard
+    kaccounts-integration
+    signon-kwallet-extension
+    signond
+
+    # RSS reader
     newsflash
   ];
 }
