41e92f79d5404152b1ffc8d611a42748e25fe4c0
Rigoberto L. Perez <rlperez@kablamo.me>
2026-08-16 23:47:12 -0400

    removed since unrequired using system.autoUpgrade

--- scripts/update-flake.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-: "${REPO_PATH:?REPO_PATH is not set}"
-: "${GIT_USERNAME:?GIT_USERNAME is not set}"
-
-cd "$REPO_PATH"
-
-exec runuser -u "$GIT_USERNAME" -- nix flake update
