0364c79160e94fb511e269bec946282534024f91
Rigoberto L. Perez <rlperez@kablamo.me>
2026-08-22 18:19:03 -0400

    add some transient style for starship

--- config/shell/fish/config.fish
+++ config/shell/fish/config.fish
@@ -49,6 +49,12 @@ if status is-interactive
     end
 
     if type -q starship
+        function starship_transient_prompt_func
+          starship module character
+        end
+        function starship_transient_rprompt_func
+          starship module time
+        end
         starship init fish --print-full-init | source
         enable_transience
     end
