0c5e6fd4866e1f98797376ee20266e408ac59526
Rigoberto L. Perez <rlperez@kablamo.me>
2026-08-22 01:58:18 -0400

    these functions are entirely disabling prompts so I guess whatever

--- config/shell/fish/config.fish
+++ config/shell/fish/config.fish
@@ -49,12 +49,6 @@ if status is-interactive
     end
 
     if type -q starship
-        function starship_transient_prompt_func
-          starship module character
-        end
-        function starship_transient_rprompt_func
-          starship module time
-        end
         starship init fish --print-full-init | source
         enable_transience
     end
