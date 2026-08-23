c211031a6858f62eecfc20502039f76f745df684
Rigoberto L. Perez <rlperez@kablamo.me>
2026-08-22 23:29:34 -0400

    make starship work in bash and refactor the config

--- users/primary-user.nix
+++ users/primary-user.nix
@@ -1,15 +1,10 @@
-{ primary_username, ... }:
+{ pkgs, primary_username, ... }:
 
 {
   home.username = primary_username;
   home.homeDirectory = "/home/${primary_username}";
   home.stateVersion = "26.05";
   home.file = {
-    # Bash
-    ".bashrc".source = ../config/shell/bash/.bashrc;
-    ".bash_profile".source = ../config/shell/bash/.bash_profile;
-    ".bash_aliases".source = ../config/shell/bash/.bash_aliases;
-
     # Environment
     ".config/shell/.env".source = ../config/shell/.env;
     ".config/shell/bash/.env".source = ../config/shell/bash/.env;
@@ -22,24 +17,76 @@
     };
   };
 
+  home.packages = with pkgs; [
+    blesh
+  ];
+
+  programs.atuin = {
+    enable = true;
+    daemon.enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    bashrcExtra = ''
+      set -a
+      source ~/.config/shell/.env
+      source ~/.config/shell/bash/.env
+      set +a
+    '';
+    historySize = 10000;
+    initExtra = ''
+      [[ $- == *i* ]] && source ${pkgs.blesh}/share/blesh/ble.sh;
+    '';
+    shellAliases = {
+      ls = "/nix/var/nix/profiles/system/sw/bin/eza";
+      l = "/nix/var/nix/profiles/system/sw/bin/eza -lah";
+      ll = "/nix/var/nix/profiles/system/sw/bin/eza -l";
+      la = "/nix/var/nix/profiles/system/sw/bin/eza -a";
+      lt = "/nix/var/nix/profiles/system/sw/bin/eza --tree";
+      lla = "/nix/var/nix/profiles/system/sw/bin/eza -la";
+      zed = "/nix/var/nix/profiles/system/sw/bin/zeditor";
+    };
+    shellOptions = [
+      "autocd"
+      "cdspell"
+      "checkwinsize"
+      "globstar"
+      "histappend"
+    ];
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.eza = {
+    enable = true;
+    enableBashIntegration = true;
+    extraOptions = [
+      "--group-directories-first"
+    ];
+    git = true;
+    icons = "auto";
+  };
+
+  programs.fastfetch = {
+    enable = true;
+  };
+
   programs.fish = {
     enable = true;
     generateCompletions = true;
     shellInit = "source /home/${primary_username}/config/fish/config.fish";
   };
 
-  programs.starship = {
-    enable = true;
-    enableFishIntegration = false;
-    presets = [ "nerd-font-symbols" "jetpack" ];
-    settings = {
-      add_newline = true;
-      follow_symlinks = true;
-    };
-  };
-
   programs.ghostty = {
     enable = true;
+    enableBashIntegration = true;
     systemd = {
       enable = true;
     };
@@ -48,6 +95,22 @@
       quit-after-last-window-closed = true;
       quit-after-last-window-closed-delay = "5m";
     };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = false;
+    enableBashIntegration = true;
+    presets = [ "nerd-font-symbols" "jetpack" ];
+    settings = {
+      add_newline = true;
+      follow_symlinks = true;
+    };
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
   };
 
   xdg.enable = true;
