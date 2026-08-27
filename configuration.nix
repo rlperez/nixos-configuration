b80a3bbb1058c90d67a341b9370b4b8363ab2dc8
Rigoberto L. Perez <rlperez@kablamo.me>
2026-08-26 20:46:08 -0400

    use approach of the nix wiki instead of idiotic ai

--- configuration.nix
+++ configuration.nix
@@ -22,28 +22,14 @@ in
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    initrd.luks.devices = {
-      slow_storage = {
-        device = "/dev/disk/by-uuid/9f476a03-d2a3-49eb-84db-fd9639212911";
-        keyFile = "/etc/luks-keys/slow_storage.key";
-      };
-
-      fast_storage = {
-        device = "/dev/disk/by-uuid/ef01f363-ae97-4918-82a2-fa1796bd521a";
-        keyFile = "/etc/luks-keys/fast_storage.key";
-      };
-    };
     kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-bore-lto-x86_64-v3;
   };
 
   environment.etc = {
-    "1password/custom_allowed_browsers" = {
-      text = ''
-        librewolf
-        brave
-      '';
-      mode = "0755";
-    };
+    crypttab.text = ''
+      slow_storage UUID=9f476a03-d2a3-49eb-84db-fd9639212911 /root/slow_storage.key
+      fast_storage UUID=ef01f363-ae97-4918-82a2-fa1796bd521a /root/fast_storage.key
+    '';
   };
 
   fileSystems = {
